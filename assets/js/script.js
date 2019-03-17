56c2224ea6e6b7bbcf87bd25d4695a238325fd6d
Karol Sliwka <hello@abovewave.co.uk>
2019-03-17 22:56:19 +0000

    validation script changed

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,10 @@ function showPage() {
 function FindMe() {
   document.getElementById("main_form").reset();
 
+  document.getElementById("town_missing").classList.remove("form-text text-muted");
+  document.getElementById("postcode_missing").classList.remove("text-muted");
+
+
 };
 
 /*  This function validate main form and checks, if any tick box has been ticked */
@@ -21,21 +25,17 @@ function StartYourTrip() {
   /* main form variables */
   var myTown = document.getElementById("mainbox_city").value;
   var myPostCode = document.getElementById("mainbox_postcode").value;
-  
+
   if (myTown = " ") {
-    document.getElementById("town_missing").classList.remove("hidden");
-    document.getElementById("town_missing").classList.add("text-muted");
-  }else {
-    document.getElementById("town_missing").classList.remove("text-muted");
+    document.getElementById("town_missing").removeClass("hidden");
+    document.getElementById("town_missing").addClass("text-muted");
   }
-  
+
   if (myPostCode = " ") {
     document.getElementById("postcode_missing").classList.remove("hidden");
     document.getElementById("postcode_missing").classList.add("text-muted");
-  }else {
-    document.getElementById("postcode_missing").classList.remove("text-muted");
   }
-  
+
 
 
   /* geo localization to find user location via clicking on "FindMe" button */
