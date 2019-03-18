5ef39ef3e0e5ccec49ecf16d9c8927fb62710d23
Karol Sliwka <hello@abovewave.co.uk>
2019-03-18 19:01:19 +0000

    empty field validation  script finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,25 +26,29 @@ function StartYourTrip() {
   var myTown = document.getElementById("mainbox_city").value;
   var myPostCode = document.getElementById("mainbox_postcode").value;
 
-  if (myTown = " ") {
-    document.getElementById("town_missing").removeClass("hidden");
-    document.getElementById("town_missing").addClass("text-muted");
+  /* check if Town input box is empty */
+  if (!myTown) {
+    document.getElementById("town_missing").classList.remove("hidden");
+    document.getElementById("town_missing").classList.add("text-muted");
   }
-
-  if (myPostCode = " ") {
+  else {
+    document.getElementById("town_missing").classList.add("hidden");
+    document.getElementById("town_missing").classList.remove("text-muted");
+  }
+  
+  /* check if Post Code input box is empty */
+  if (!myPostCode) {
     document.getElementById("postcode_missing").classList.remove("hidden");
     document.getElementById("postcode_missing").classList.add("text-muted");
   }
-
-
+  else {
+    document.getElementById("postcode_missing").classList.add("hidden");
+    document.getElementById("postcode_missing").classList.remove("text-muted");
+  }
 
   /* geo localization to find user location via clicking on "FindMe" button */
   var myFindMe = "Find Me!";
 
-
   /* check if any of tick boxes has been clicked 'checked' */
-
-
-  console.log(myTown, myPostCode);
 
 }
