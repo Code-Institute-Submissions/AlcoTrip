39a20f15367eab53e507e588a1df2649735a7c9b
Karol Sliwka <hello@abovewave.co.uk>
2019-03-24 13:47:54 +0000

    localization icon added

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,7 +39,8 @@ function FindMe() {
 
   /* test value */
   myTown.value = "Wellingborough";
-  myPostCode.value = "NN82DF";
+  myPostCode.value = "NN82DF"; 
+
 
 }
 /* _____________________________________________________________________________
