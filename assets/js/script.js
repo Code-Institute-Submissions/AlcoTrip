6c4d498638dc4684df42fce8f27f7c5ea4f76286
Karol Sliwka <hello@abovewave.co.uk>
2019-10-14 17:49:43 +0000

    Major check - Jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,12 +12,13 @@ function showPage() {
 }
 
 // Check all required fields and start trip
-function StartYourTrip() {
+
+$('#start_trip_button').click(function() {
 
   CheckErrors();
   CheckSpaces();
 
-}
+});
 
 
 function CheckErrors() {
@@ -53,57 +54,60 @@ function CheckErrors() {
   }
 }
 
-
-
 function CheckSpaces() {
 
 
-  let myPostCode = $("#mainbox_postcode").val();
-  console.log(myPostCode);
-  if (!myPostCode) {
-    myPostCode = "nothing";
-  }
-
-
-  $('#postcode_sidebar').html(myPostCode);
-
+  let myPostCode = $('#mainbox_postcode');
+  let resultPostcode = myPostCode.val();
+  $('#start_trip_button').val(resultPostcode);
 
 }
 
 // Find user location based on geolocalization from google
-$('#findme').click(function initMap() {
+$('#findme_button').click(function() {
 
 
   $("#mainbox_postcode").removeClass("missing_e");
   $("#postcode_error").addClass("hidden");
+
+  $("#mainbox_postcode").val('something');
+
+  let myPostCode = $('#mainbox_postcode');
 
 
 });
 
 
+
 // Clear postcode field and remove error message
-function ClearPostcode() {
+
+
+$('#clear_postcode').click(function() {
   $("#mainbox_postcode").val('');
   $("#mainbox_postcode").removeClass("missing_e");
   $("#postcode_error").addClass("hidden");
-}
+});
+
+
 // Clear all checboxes and hidden error message
-function ClearCheckboxes() {
+$('#unselect_all_checkbox').click(function() {
   $("#styled-checkbox-1 ,#styled-checkbox-2 ,#styled-checkbox-3").prop("checked", false);
   // clear errors
   $("#tickbox_missing").addClass("hidden");
   $(".c_boxes").removeClass("missing_e");
-}
+});
+
 // Select all checboxes and clear hidden error message
-function SelectAll() {
+$('#select_all_checkbox').click(function() {
   $("#styled-checkbox-1 ,#styled-checkbox-2 ,#styled-checkbox-3").prop("checked", true);
   // clear errors
   $("#tickbox_missing").addClass("hidden");
   $("#tickbox_missing").removeClass("text-muted1");
   $(".c_boxes").removeClass("missing_e");
-}
+});
+
 // Collaspe or expand sidebar
-function collapseSideBar() {
+$('#sidebar_collapse_icon').click(function() {
   let toggle_q = document.getElementById("sidebar_blue").clientWidth;
   let tmpAnimation = 0;
   let element = $("#sidebar_collapse_icon");
@@ -215,50 +219,34 @@ function collapseSideBar() {
       break;
     default:
   }
-}
+});
 
 
-function clear() {
+$('#').click(function() {
   $("#hint_show ,#ssb_icons").addClass("hidden");
   $("#hint_hide, #main_sidebar, #sidebar_separate_line, #logotype_sidebar, #sidebar_copyrights, #sidebar_logo_top").removeClass("hidden");
   $("#slider, #sidebar_separate_line, #logotype_sidebar, #sidebar_copyrights, #sidebar_buttons, #sidebar_logo_top").fadeTo(250, 1);
-}
-
-
-// TURN OFF OR ON MARKERS
-function MarkerOnOff() {
-
-
-  $("#red_marker").click(function() {
-    if ($(this).children("span").hasClass("hidden")) {
-      $(this).children("span.tiptext_s").removeClass("hidden");
-      $(this).children("img").attr("src", "assets/images/icons/marker_off.png");
-    }
-    else if ($(this).children("span").hasClass("tiptext_s")) {
-      $(this).children("span.tiptext_s").addClass("hidden");
-      $(this).children("img").attr("src", "assets/images/icons/marker_red.png");
-    }
-  });
-}
+});
 
 
 
 // Close map function ( red corss - button )
-function closeMap() {
+$('#exit_icon').click(function() {
   $("#main_page_container").removeClass("hidden");
   $("#map_container").addClass("hidden");
   $("#footer_main").removeClass("hidden");
-}
+});
+
 // Reset sliders to "50 Miles" - value
-function resetRadius() {
+$('#reset_sliders').ckick(function() {
   $("#clubs_dis_range, #pubs_dis_range, #bars_dis_range").val("50");
   $("#clubs_distance").html($("#clubs_dis_range").val() + "&nbsp;" + "Miles");
   $("#pubs_distance").html($("#pubs_dis_range").val() + "&nbsp;" + "Miles");
   $("#bars_distance").html($("#bars_dis_range").val() + "&nbsp;" + "Miles");
-}
+});
 
 
-function downloadMe() {
+$('#download').click(function() {
   // download as pdf function
 
   // when downloaded show allert box
@@ -270,7 +258,8 @@ function downloadMe() {
     showConfirmButton: false,
     timer: 2500
   });
-}
+});
+
 // Sidebar sliders - listeners
 $(document).on("input change", "#clubs_dis_range", function() {
   $("#clubs_distance").html($(this).val() + "&nbsp;" + "Miles");
