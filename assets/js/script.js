f8a6c492c93e0a2c84ca32bb59121ea986173cca
Karol Sliwka <hello@abovewave.co.uk>
2019-10-18 20:44:20 +0000

    Sidebar.js file created

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,14 @@
 //page loader function
-/* global $ , google , navigator, global map, pos, global Swal, browser_height, mymap, L, club_radius, createMarker, service, infowindow, jsPDF, myPostCode, checked_clubs, checked_bars, checked_pubs */
+/* global $, global Swal */
 
 function myFunction() {
   let loader = setTimeout(showPage(), 3000);
 }
 
-
-
 function showPage() {
   $("#loader, #whole_page").css("display", "none");
   $("#page_loader").css("display", "block");
 }
-
-
 
 // Check all required fields,
 // pass the postcode value to H3 on map page and start trip.
@@ -57,31 +53,22 @@ $('#start_trip_button').click(function() {
 
 });
 
-
-
 // Find user location based on geolocalization from google
 $('#findme_button').click(function() {
 
-
   $("#mainbox_postcode").removeClass("missing_e");
   $("#postcode_error").addClass("hidden");
-  $("#mainbox_postcode").val('something');
-
+  let g_location = "your postcode";
+  $("#mainbox_postcode").val(g_location);
 
 });
 
-
-
 // Clear postcode field and remove error message
-
-
 $('#clear_postcode').click(function() {
   $("#mainbox_postcode").val('');
   $("#mainbox_postcode").removeClass("missing_e");
   $("#postcode_error").addClass("hidden");
 });
-
-
 
 // Clear all checboxes and hidden error message
 $('#unselect_all_checkbox').click(function() {
@@ -90,8 +77,6 @@ $('#unselect_all_checkbox').click(function() {
   $("#tickbox_missing").addClass("hidden");
   $(".c_boxes").removeClass("missing_e");
 });
-
-
 
 // Select all checboxes and clear hidden error message
 $('#select_all_checkbox').click(function() {
@@ -102,131 +87,13 @@ $('#select_all_checkbox').click(function() {
   $(".c_boxes").removeClass("missing_e");
 });
 
-
-
-// Collaspe or expand sidebar
-$('#sidebar_collapse_icon').click(function() {
-  let toggle_q = document.getElementById("sidebar_blue").clientWidth;
-  let tmpAnimation = 0;
-  let element = $("#sidebar_collapse_icon");
-  let browser_height = $(window).height();
-
-  switch (toggle_q) {
-    // when wide sidebar is 360px width
-    case 360:
-      document.documentElement.style
-        .setProperty("--sBar_width", "70px");
-      tmpAnimation = tmpAnimation - 180;
-      $({ degrees: tmpAnimation - 180 }).animate({ degrees: tmpAnimation }, {
-        duration: 300,
-        step: function(now) {
-          element.css({
-            transform: 'rotate(' + now + 'deg)'
-          });
-        }
-      });
-      $("#hint_hide, #main_sidebar, #sidebar_logo_top").addClass("hidden");
-      $("#hint_show, #ssb_icons").removeClass("hidden");
-      $("#slider, #sidebar_separate_line, #logotype_sidebar, #sidebar_copyrights, #sidebar_buttons, #sidebar_logo_top").fadeTo(250, 0,
-        function() {
-          $("#sidebar_separate_line, #logotype_sidebar, #sidebar_copyrights").addClass("hidden");
-        }
-      );
-      if (browser_height <= 750) {
-        $("#logotype_sidebar").addClass("hidden");
-      }
-      else {
-        $("#logotype_sidebar").removeClass("hidden");
-      }
-      break;
-
-
-      // when wide sidebar is 320px width
-    case 320:
-      document.documentElement.style
-        .setProperty("--sBar_width", "68px");
-      tmpAnimation = tmpAnimation - 180;
-      $({ degrees: tmpAnimation - 180 }).animate({ degrees: tmpAnimation }, {
-        duration: 300,
-        step: function(now) {
-          element.css({
-            transform: 'rotate(' + now + 'deg)'
-          });
-        }
-      });
-      $("#hint_hide, #main_sidebar, #sidebar_logo_top").addClass("hidden");
-      $("#hint_show, #ssb_icons").removeClass("hidden");
-      $("#slider, #sidebar_separate_line, #logotype_sidebar, #sidebar_copyrights, #sidebar_buttons, #sidebar_logo_top").fadeTo(250, 0,
-        function() {
-          $("#sidebar_separate_line, #logotype_sidebar, #sidebar_copyrights").addClass("hidden");
-        }
-      );
-      if (browser_height <= 750) {
-        $(".collapse_sidebar").css("margin-left", "0px");
-        $("#logotype_sidebar").addClass("hidden");
-      }
-      else {
-        $("#logotype_sidebar").removeClass("hidden");
-      }
-      break;
-      // when little sidebar is 70px width
-    case 70:
-      document.documentElement.style
-        .setProperty("--sBar_width", "360px");
-      tmpAnimation = tmpAnimation - 0;
-      $({ degrees: tmpAnimation + 180 }).animate({ degrees: tmpAnimation }, {
-        duration: 300,
-        step: function(now) {
-          element.css({
-            transform: 'rotate(' + now + 'deg)'
-          });
-        }
-      });
-
-
-      if (browser_height <= 750) {
-        $(".collapse_sidebar").css("margin-left", "20px");
-        $("#logotype_sidebar").addClass("hidden");
-      }
-      else {
-        $("#logotype_sidebar").removeClass("hidden");
-      }
-      break;
-      // when little sidebar is 68px width
-
-    case 68:
-      document.documentElement.style
-        .setProperty("--sBar_width", "320px");
-      tmpAnimation = tmpAnimation - 0;
-      $({ degrees: tmpAnimation + 180 }).animate({ degrees: tmpAnimation }, {
-        duration: 300,
-        step: function(now) {
-          element.css({
-            transform: 'rotate(' + now + 'deg)'
-          });
-        }
-      });
-      $("#hint_show ,#ssb_icons").addClass("hidden");
-      $("#hint_hide, #main_sidebar, #sidebar_separate_line, #logotype_sidebar, #sidebar_copyrights, #sidebar_logo_top").removeClass("hidden");
-      $("#slider, #sidebar_separate_line, #logotype_sidebar, #sidebar_copyrights, #sidebar_buttons, #sidebar_logo_top").fadeTo(250, 1);
-      if (browser_height <= 750) {
-        $(".collapse_sidebar").css("margin-left", "20px");
-        $("#logotype_sidebar").addClass("hidden");
-      }
-      else {}
-      break;
-    default:
-  }
-});
-
 // Close map function ( red corss - button )
 $('#exit_icon').click(function() {
   $('#main_page_container').removeClass('hidden');
   $('#map_container').addClass('hidden');
   $('#footer_main').removeClass('hidden');
+  $("#mainbox_postcode").val("");
 });
-
-
 
 // Reset sliders to "50 Miles" - value
 $("#reset_sliders").click(function() {
@@ -235,8 +102,6 @@ $("#reset_sliders").click(function() {
   $("#pubs_distance").html($("#pubs_dis_range").val() + "&nbsp;" + "Miles");
   $("#bars_distance").html($("#bars_dis_range").val() + "&nbsp;" + "Miles");
 });
-
-
 
 $('#download').click(function() {
   // download as pdf function
@@ -250,8 +115,6 @@ $('#download').click(function() {
     timer: 2500
   });
 });
-
-
 
 // Sidebar sliders - listeners
 $(document).on("input change", "#clubs_dis_range", function() {
---
+++ assets/js/sidebar.js
@@ -0,0 +1,112 @@
+//page loader function
+/* global $ */
+
+
+// Collaspe or expand sidebar
+$('#sidebar_collapse_icon').click(function() {
+    let toggle_q = document.getElementById("sidebar_blue").clientWidth;
+    let tmpAnimation = 0;
+    let element = $("#sidebar_collapse_icon");
+    let browser_height = $(window).height();
+    switch (toggle_q) {
+        // when wide sidebar is 360px width
+        case 360:
+            document.documentElement.style
+                .setProperty("--sBar_width", "70px");
+            tmpAnimation = tmpAnimation - 180;
+            $({ degrees: tmpAnimation - 180 }).animate({ degrees: tmpAnimation }, {
+                duration: 300,
+                step: function(now) {
+                    element.css({
+                        transform: 'rotate(' + now + 'deg)'
+                    });
+                }
+            });
+            $("#hint_hide, #main_sidebar, #sidebar_logo_top").addClass("hidden");
+            $("#hint_show, #ssb_icons").removeClass("hidden");
+            $("#slider, #sidebar_separate_line, #logotype_sidebar, #sidebar_copyrights, #sidebar_buttons, #sidebar_logo_top").fadeTo(250, 0,
+                function() {
+                    $("#sidebar_separate_line, #logotype_sidebar, #sidebar_copyrights").addClass("hidden");
+                }
+            );
+            if (browser_height <= 750) {
+                $("#logotype_sidebar").addClass("hidden");
+            }
+            else {
+                $("#logotype_sidebar").removeClass("hidden");
+            }
+            break;
+            // when wide sidebar is 320px width
+        case 320:
+            document.documentElement.style
+                .setProperty("--sBar_width", "68px");
+            tmpAnimation = tmpAnimation - 180;
+            $({ degrees: tmpAnimation - 180 }).animate({ degrees: tmpAnimation }, {
+                duration: 300,
+                step: function(now) {
+                    element.css({
+                        transform: 'rotate(' + now + 'deg)'
+                    });
+                }
+            });
+            $("#hint_hide, #main_sidebar, #sidebar_logo_top").addClass("hidden");
+            $("#hint_show, #ssb_icons").removeClass("hidden");
+            $("#slider, #sidebar_separate_line, #logotype_sidebar, #sidebar_copyrights, #sidebar_buttons, #sidebar_logo_top").fadeTo(250, 0,
+                function() {
+                    $("#sidebar_separate_line, #logotype_sidebar, #sidebar_copyrights").addClass("hidden");
+                }
+            );
+            if (browser_height <= 750) {
+                $(".collapse_sidebar").css("margin-left", "0px");
+                $("#logotype_sidebar").addClass("hidden");
+            }
+            else {
+                $("#logotype_sidebar").removeClass("hidden");
+            }
+            break;
+            // when little sidebar is 70px width
+        case 70:
+            document.documentElement.style
+                .setProperty("--sBar_width", "360px");
+            tmpAnimation = tmpAnimation - 0;
+            $({ degrees: tmpAnimation + 180 }).animate({ degrees: tmpAnimation }, {
+                duration: 300,
+                step: function(now) {
+                    element.css({
+                        transform: 'rotate(' + now + 'deg)'
+                    });
+                }
+            });
+            if (browser_height <= 750) {
+                $(".collapse_sidebar").css("margin-left", "20px");
+                $("#logotype_sidebar").addClass("hidden");
+            }
+            else {
+                $("#logotype_sidebar").removeClass("hidden");
+            }
+            break;
+            // when little sidebar is 68px width
+        case 68:
+            document.documentElement.style
+                .setProperty("--sBar_width", "320px");
+            tmpAnimation = tmpAnimation - 0;
+            $({ degrees: tmpAnimation + 180 }).animate({ degrees: tmpAnimation }, {
+                duration: 300,
+                step: function(now) {
+                    element.css({
+                        transform: 'rotate(' + now + 'deg)'
+                    });
+                }
+            });
+            $("#hint_show ,#ssb_icons").addClass("hidden");
+            $("#hint_hide, #main_sidebar, #sidebar_separate_line, #logotype_sidebar, #sidebar_copyrights, #sidebar_logo_top").removeClass("hidden");
+            $("#slider, #sidebar_separate_line, #logotype_sidebar, #sidebar_copyrights, #sidebar_buttons, #sidebar_logo_top").fadeTo(250, 1);
+            if (browser_height <= 750) {
+                $(".collapse_sidebar").css("margin-left", "20px");
+                $("#logotype_sidebar").addClass("hidden");
+            }
+            else {}
+            break;
+        default:
+    }
+});
