1d04b24adb0b7c2dad2fc3b6055fce3beeda9fad
Karol Sliwka <hello@abovewave.co.uk>
2019-10-24 13:58:32 +0000

    PostcodeIo API request finished

--- assets/python/python.py
+++ assets/python/python.py
@@ -2,16 +2,17 @@
 https://github.com/samlopezf/Postcode-API-Lookup-Validation-Python-3-Implementation-Tutorial/blob/master/postcode-api-tutorial.py
 '''
 
-import urllib.request as req
+import urllib.request as myRequest
 import json
 
-xpostcode = 'NN82DF'
+xpostcode =  "nn84tj"
 
 def loadJsonResponse(url):
-    return json.loads(req.urlopen(url).read())['result']
+    return json.loads(myRequest.urlopen(url).read())['result']
 
 def queryPostcode(postcode):
     url = 'https://api.postcodes.io/postcodes?q={}'.format(postcode)
     return loadJsonResponse(url)
-
-print(queryPostcode(xpostcode))
+    
+myLat = (queryPostcode(xpostcode)[0]['latitude'])
+myLong = (queryPostcode(xpostcode)[0]['longitude'])
