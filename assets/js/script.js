304292a5dd59869a92e065be8a78b8653af711f5
Karol Sliwka <hello@abovewave.co.uk>
2020-01-23 19:34:53 +0000

    Google places, searchby query test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
 //page loader function
-/* global $, global Swal, google, createMarker, request, callback, i, place*/
+/* global $, global Swal, google, clubs_range */
 /* global, gLat, gLong */
 let myLat, myLong;
 
+clubs_range = $('#clubs_dis_range').val();
+//pubs_range = $('#pubs_dis_range').val();
+//bars_range = $('#bars_dis_range').val();
 
 // Find user location based on geolocalization from google
 $('#findme_button').click(function() {
@@ -14,7 +17,6 @@ $('#findme_button').click(function() {
             $.get(encodeURI("https://api.postcodes.io/postcodes?lon=" + geolocLong + "&lat=" + geolocLat))
                 .done(function(data) {
                     let geolocPostcode = (data.result[0]['postcode']);
-                    console.log(geolocPostcode);
                     $("#mainbox_postcode").val(geolocPostcode);
                 });
         });
@@ -38,151 +40,11 @@ $('#select_all_checkbox').click(function() {
 
 // Check all required fields,
 // pass the postcode value to H3 on map page and start trip.
-$('#start_trip_button').click(function initMap() {
-
-    let service;
-    let infowindow;
-
-    infowindow = new google.maps.InfoWindow();
-
-    let myPostCode, checked_clubs, checked_bars, checked_pubs;
-
-    myPostCode = $("#mainbox_postcode").val();
-    checked_clubs = $("#styled-checkbox-1").prop("checked");
-    checked_bars = $("#styled-checkbox-3").prop("checked");
-    checked_pubs = $("#styled-checkbox-2").prop("checked");
-
-    if (myPostCode == "") {
-        mainPostcodeError();
-    }
-    else if (checked_clubs == false && checked_pubs == false && checked_bars == false) {
-        $("#tickbox_missing").removeClass("hidden");
-        $(".c_boxes").addClass("missing_e");
-        $('html, body').animate({
-            scrollTop: ($('#tickbox_missing_err').offset().top)
-        }, 500);
-    }
-    else {
-
-        // passing value from postcode field to h3 selector
-        let myPostcode = $("#mainbox_postcode").val();
-        myPostcode = myPostcode.replace(/\s/g, "");
-        myPostcode = String(myPostcode.toUpperCase());
-
-        //postcode validation
-        event.preventDefault();
-        $.get(encodeURI("https://api.postcodes.io/postcodes/" + myPostcode + "/validate"))
-            .done(function(data) {
-
-                let postcodeValidation = data['result'];
-                if (postcodeValidation) {
-                    $("#postcode_sidebar").html(myPostcode);
-
-                    // Variables
-                    let map;
-                    $.get(encodeURI("https://api.postcodes.io/postcodes/" + myPostcode))
-                        .done(function(data) {
-                            myLat = data.result['latitude'];
-                            myLong = data.result['longitude'];
-                            // current position from postcode - main variable
-                            let myLocation = { lat: myLat, lng: myLong };
-                            // new map
-                            let mapOptions = {
-                                zoom: 15,
-                                maxZoom: 18,
-                                minZoom: 10,
-                                center: myLocation,
-                                mapTypeId: 'roadmap'
-                            };
-
-                            // MARKERS
-                            let marker_my_pos = {
-                                url: 'https://www.abovewave.kylos.pl/aclotrip_project/marker_mypos_white.png',
-                                scaledSize: new google.maps.Size(40, 64),
-                            };
-                            let marker_clubs_pos = {
-                                url: './assets/images/icons/marker_green.png',
-                                scaledSize: new google.maps.Size(28, 45),
-                            };
-                            let marker_pubs_pos = {
-                                url: './assets/images/icons/marker_orange.png',
-                                scaledSize: new google.maps.Size(28, 45),
-                            };
-                            let marker_bars_pos = {
-                                url: './assets/images/icons/marker_blue.png',
-                                scaledSize: new google.maps.Size(28, 45),
-                            };
-
-                            // CREATING MAP WITH OPTIONS
-                            map = new google.maps.Map(document.getElementById('map'),
-                                mapOptions);
-
-                            // CREATE CURRENT POSITION MARKER
-                            let yourPosition = new google.maps.Marker({
-                                icon: marker_my_pos,
-                                position: myLocation,
-                                animation: google.maps.Animation.DROP,
-                                map: map,
-                            });
-
-                            //SHOW NAME FOR CURRENT POSITION MARKER IN BOX
-                            google.maps.event.addListener(yourPosition, 'click', function() {
-                                infowindow.setContent("You're here");
-                                infowindow.open(map, this);
-                            });
-
-                            // Create the places service.
-                            var service = new google.maps.places.PlacesService(map);
-                            var getNextPage = null;
-                            var moreButton = document.getElementById('more');
-                            moreButton.onclick = function() {
-                                moreButton.disabled = true;
-                                if (getNextPage) getNextPage();
-                            };
-
-                            // Perform a nearby search.
-                            service.nearbySearch({ location: myLocation, radius: 40, type: ['store'] },
-                                function(results, status, pagination) {
-                                    if (status !== 'OK') return;
-
-                                    createMarkers(results);
-                                    moreButton.disabled = !pagination.hasNextPage;
-                                    getNextPage = pagination.hasNextPage && function() {
-                                        pagination.nextPage();
-                                    };
-                                });
-
-
-                            function createMarkers(places) {
-                                let bounds = new google.maps.LatLngBounds();
-                                for (i = 0, place; place = places[i]; i++) {
-                                    var marker = new google.maps.Marker({
-                                        map: map,
-                                        icon: marker_clubs_pos,
-                                        title: place.name,
-                                        position: place.geometry.location
-                                    });
-
-                                    bounds.extend(place.geometry.location);
-                                }
-                                map.fitBounds(bounds);
-                            }
-
-                        });
-                    // Go to Map
-                    $("#main_page_container, #postcode_error, #tickbox_missing, #footer_main").addClass("hidden");
-                    $("#map_container").addClass("map_main");
-                    $("#map_container").removeClass("hidden");
-                    $("#mainbox_postcode, .c_boxes").removeClass("missing_e");
-                    $("#styled-checkbox-1 ,#styled-checkbox-2 ,#styled-checkbox-3").prop("checked", false);
-                }
-                else {
-                    mainPostcodeError();
-                    notValidPostcode();
-                }
-            });
-    }
+$('#start_trip_button').click(function() {
+    initMap();
 });
+
+
 // Sidebar sliders - listeners
 $(document).on("input change", "#clubs_dis_range", function() {
     $("#clubs_distance").html($(this).val() + "&nbsp;" + "Miles");
@@ -202,9 +64,12 @@ $("#reset_sliders").click(function() {
 });
 // Apply sliders changes when accepted
 $('#apply_sliders').click(function() {
-    let clubs_range = $('#clubs_dis_range').val();
+    clubs_range = $('#clubs_dis_range').val();
     let pubs_range = $('#pubs_dis_range').val();
     let bars_range = $('#bars_dis_range').val();
+
+
+    initMap();
 });
 // donwload trip screenshot 
 $('#download').click(function() {
@@ -245,7 +110,9 @@ function mainPostcodeError() {
         scrollTop: ($('#postcode_missing').offset().top)
     }, 500);
 }
-// sweet alert for geolocation error
+
+// sweet alert for geolocation error 
+/*
 function geolocationError() {
     Swal.fire({
         position: 'warning',
@@ -255,4 +122,144 @@ function geolocationError() {
         showConfirmButton: false,
         timer: 2000
     });
+}*/
+
+
+function initMap() {
+    let map, service, infowindow;
+    let postcodeValidation, myPostcode;
+    let myPostCode, checked_clubs, checked_bars, checked_pubs;
+
+
+
+    // MARKERS
+    const marker_my_pos = {
+        url: 'https://www.abovewave.kylos.pl/aclotrip_project/marker_mypos_white.png',
+        scaledSize: new google.maps.Size(40, 64),
+    };
+    const marker_clubs_pos = {
+        url: './assets/images/icons/marker_green.png',
+        scaledSize: new google.maps.Size(28, 45),
+    };
+    const marker_pubs_pos = {
+        url: './assets/images/icons/marker_orange.png',
+        scaledSize: new google.maps.Size(28, 45),
+    };
+    const marker_bars_pos = {
+        url: './assets/images/icons/marker_blue.png',
+        scaledSize: new google.maps.Size(28, 45),
+    };
+
+    myPostCode = $("#mainbox_postcode").val();
+    checked_clubs = $("#styled-checkbox-1").prop("checked");
+    checked_bars = $("#styled-checkbox-3").prop("checked");
+    checked_pubs = $("#styled-checkbox-2").prop("checked");
+
+    if (myPostCode == "") {
+        mainPostcodeError();
+    }
+    else if (checked_clubs == false && checked_pubs == false && checked_bars == false) {
+        $("#tickbox_missing").removeClass("hidden");
+        $(".c_boxes").addClass("missing_e");
+        $('html, body').animate({
+            scrollTop: ($('#tickbox_missing_err').offset().top)
+        }, 500);
+    }
+    else {
+
+        // passing value from postcode field to h3 selector
+        myPostcode = $("#mainbox_postcode").val();
+        myPostcode = myPostcode.replace(/\s/g, "");
+        myPostcode = String(myPostcode.toUpperCase());
+
+        //postcode validation
+        event.preventDefault();
+        $.get(encodeURI("https://api.postcodes.io/postcodes/" + myPostcode + "/validate"))
+            .done(function(data) {
+
+                postcodeValidation = data['result'];
+                if (postcodeValidation) {
+                    $("#postcode_sidebar").html(myPostcode);
+
+                    // Variables
+                    $.get(encodeURI("https://api.postcodes.io/postcodes/" + myPostcode))
+                        .done(function(data) {
+                            myLat = data.result['latitude'];
+                            myLong = data.result['longitude'];
+                            // current position from postcode - main variable
+                            let myLocation = { lat: myLat, lng: myLong };
+                            // new map
+                            let mapOptions = {
+                                zoom: 15,
+                                maxZoom: 18,
+                                minZoom: 4, //10
+                                center: myLocation,
+                                mapTypeId: 'roadmap'
+                            };
+
+                            infowindow = new google.maps.InfoWindow();
+                            // CREATING MAP WITH OPTIONS
+                            map = new google.maps.Map(document.getElementById('map'),
+                                mapOptions);
+
+
+                            // CREATE CURRENT POSITION MARKER
+                            let yourPosition = new google.maps.Marker({
+                                icon: marker_my_pos,
+                                position: myLocation,
+                                animation: google.maps.Animation.DROP,
+                                map: map,
+                            });
+
+                            //SHOW NAME FOR CURRENT POSITION MARKER IN BOX
+                            google.maps.event.addListener(yourPosition, 'click', function() {
+                                infowindow.setContent("You're here");
+                                infowindow.open(map, this);
+                            });
+
+                            var request = {
+                                location: myLocation,
+                                radius: clubs_range,
+                                query: 'night_club'
+                            };
+
+                            service = new google.maps.places.PlacesService(map);
+                            service.textSearch(request, callback);
+
+                            function callback(results, status) {
+                                if (status == google.maps.places.PlacesServiceStatus.OK) {
+                                    for (var i = 0; i < results.length; i++) {
+                                        var place = results[i];
+                                        createMarker(results[i]);
+                                    }
+                                }
+                            }
+
+                            function createMarker(place) {
+                                var marker = new google.maps.Marker({
+                                    map: map,
+                                    icon: marker_bars_pos,
+                                    animation: google.maps.Animation.Drop,
+                                    position: place.geometry.location
+                                });
+
+                                google.maps.event.addListener(marker, 'click', function() {
+                                    infowindow.setContent(place.name);
+                                    infowindow.open(map, this);
+                                });
+                            }
+                        });
+                    // Go to Map
+                    $("#main_page_container, #postcode_error, #tickbox_missing, #footer_main").addClass("hidden");
+                    $("#map_container").addClass("map_main");
+                    $("#map_container").removeClass("hidden");
+                    $("#mainbox_postcode, .c_boxes").removeClass("missing_e");
+                    $("#styled-checkbox-1 ,#styled-checkbox-2 ,#styled-checkbox-3").prop("checked", false);
+                }
+                else {
+                    mainPostcodeError();
+                    notValidPostcode();
+                }
+            });
+    }
 }
