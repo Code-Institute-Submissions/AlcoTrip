368b7ca7e094c7aef48704eb2a79f06613aa5af4
Karol Sliwka <hello@abovewave.co.uk>
2020-02-02 20:53:40 +0000

    Sidebar functions moved and editef from script.js file

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -11,61 +11,110 @@ console.log(browserWidth);
  * This function will hide elements on side bar
  */
 function hideElements() {
-    $('#collapse_icon').html('show');
-    $('.sidebar_elements').addClass('hidden');
+    $("#collapse_icon").html("show");
+    $(".sidebar_elements").addClass("hidden").fadeOut(400, 0, function() {
+        // Animation complete.
+    });
+    $("#cross").removeClass("hidden").fadeTo(400, 1, "");
 }
+
 /**
  * This function will show elements on side bar
  */
 function showElements() {
-    $('#collapse_icon').html('hide');
-    $('.sidebar_elements').removeClass('hidden');
+    $("#collapse_icon").html("hide");
+    $(".sidebar_elements").removeClass("hidden").fadeTo(400, 1, "");
+    $("#cross").addClass("hidden").fadeTo(400, 0, "");
 }
 
+/**
+ * This functions will change label value based on slider postion (on change)
+ */
+$(document).on("input change", "#clubs_dis_range", function() {
+    $("#clubs_distance").html($(this).val() + " Miles");
+});
+$(document).on("input change", "#pubs_dis_range", function() {
+    $("#pubs_distance").html($(this).val() + " Miles");
+});
+$(document).on("input change", "#bars_dis_range", function() {
+    $("#bars_distance").html($(this).val() + " Miles");
+});
+
+/**
+ * This function is resetting all sliders value to 10.0
+ */
+$("#reset_sliders").click(function() {
+    $("#clubs_dis_range, #pubs_dis_range, #bars_dis_range").val("");
+    $("#clubs_distance").html($("#clubs_dis_range").val() + "&nbsp;" + "Miles");
+    $("#pubs_distance").html($("#pubs_dis_range").val() + "&nbsp;" + "Miles");
+    $("#bars_distance").html($("#bars_dis_range").val() + "&nbsp;" + "Miles");
+});
+
+/**
+ * This function will run "Map function from script.js file
+ */
+$("#apply_sliders").click(function() {
+    initMap();
+});
 
 
-
-
-
-
-
+/**
+ * This function will change classes for webite elements - take user back to main page
+ */
+$(".go_back").click(function() {
+    $(".cross_hide").removeClass("hidden");
+    $("#map_container").addClass("hidden");
+    $("#mainbox_postcode").val("");
+    $(".styled-checkbox").prop("checked", false);
+    let tmpAnimation = 0;
+    let element = $("#sidebar_collapse_icon");
+    document.documentElement.style
+        .setProperty("--sBar_width", "70px");
+    $({ degrees: tmpAnimation + 180 }).animate({ degrees: tmpAnimation }, {
+        duration: 300,
+        step: function(now) {
+            element.css({
+                transform: "rotate(" + now + "deg)"
+            });
+        }
+    });
+    hideElements();
+});
 
 /**
  * This function will change the size of the sidebar when width point is match
  */
-$('#sidebar_collapse_icon').click(function() {
+$("#sidebar_collapse_icon").click(function() {
     let toggle_q = $("#sidebar_blue").width();
     let tmpAnimation = 0;
     let element = $("#sidebar_collapse_icon");
-
-
     switch (toggle_q) {
-        case 360:
+        case 70:
             document.documentElement.style
-                .setProperty("--sBar_width", "70px");
+                .setProperty("--sBar_width", "360px");
             tmpAnimation = tmpAnimation - 180;
             $({ degrees: tmpAnimation - 180 }).animate({ degrees: tmpAnimation }, {
                 duration: 300,
                 step: function(now) {
                     element.css({
-                        transform: 'rotate(' + now + 'deg)'
-                    });
-                }
-            });
-            hideElements();
-            break;
-        case 70:
-            document.documentElement.style
-                .setProperty("--sBar_width", "360px");
-            $({ degrees: tmpAnimation + 180 }).animate({ degrees: tmpAnimation }, {
-                duration: 300,
-                step: function(now) {
-                    element.css({
-                        transform: 'rotate(' + now + 'deg)'
+                        transform: "rotate(" + now + "deg)"
                     });
                 }
             });
             showElements();
+            break;
+        case 360:
+            document.documentElement.style
+                .setProperty("--sBar_width", "70px");
+            $({ degrees: tmpAnimation + 180 }).animate({ degrees: tmpAnimation }, {
+                duration: 300,
+                step: function(now) {
+                    element.css({
+                        transform: "rotate(" + now + "deg)"
+                    });
+                }
+            });
+            hideElements();
             break;
     }
 });
